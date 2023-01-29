2e2c1f6b09d774b3deb4d95f8bd8ce7b01ed41a7
Homework3 <tradervdovin@gmail.com>
2023-01-29 16:49:42 +0500

    ДЗ 11

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     task2();
     task3();}
     public static void task1 (){
-        System.out.println("Задача № 1");
+        System.out.println("Задача 1");
         String firstName = "Ivan ";
         String middleName = "Ivanovich";
         String lastName = "Ivanov ";
@@ -13,7 +13,7 @@ public class Main {
 
     }
     public static void task2 (){
-        System.out.println("Задача № 2");
+        System.out.println("Задача 2");
         String firstName = "Ivan ";
         String middleName = "Ivanovich";
         String lastName = "Ivanov ";
@@ -21,7 +21,7 @@ public class Main {
         System.out.println(fullName.toUpperCase());
     }
     public static void task3 (){
-        System.out.println("Задача № 3");
+        System.out.println("Задача 3");
         String fullName = "Иванов Семён Семёнович";
         String fullNameTrue = fullName.replace("Иванов Семён Семёнович", "Иванов Семен Семенович");
         System.out.println(fullNameTrue);
